e1b2e80814eff85cb5d3cdc8216b50b09895d456
Vinit Kumar <vinit.ad.ad@gmail.com>
2026-07-27 17:40:03 +0530

    Update settings

--- setting.php
+++ setting.php
@@ -1,3 +1,7 @@
 <?php
+define('BASE_PATH', __DIR__ . '/');
 
-define("BASE_URL", "http://local.root.com/spedition-new/");
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$host = $_SERVER['HTTP_HOST'];
+$scriptDir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+define('BASE_URL', $protocol . $host . $scriptDir . '/');
